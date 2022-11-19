5db478c34e8e6723a5fbb146bb1e2af53fa05b5a
Chaohreum <pcor4650@gmail.com>
2022-11-19 23:22:01 +0900

    02 자바스크립트 개발환경

---
+++ 02.Javascript_basic/02_async와defer차이.js
@@ -0,0 +1,12 @@
+// nodejs.org에서 node 설치
+
+console.log('Hello World');
+
+// 자바스크립트 공식 사이트 : ecma-international.org
+// 개발자가 주로 방문하는 사이트 : developer.mozilla.org
+
+
+// async vs defer
+// defer가 좋은 옵션이다!!!!
+
+'use strict';   // 바닐라 자바스크립트로 개발 시 제일 위에 선언해주는게 좋다
